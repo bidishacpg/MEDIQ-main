14b1269055069cb7860cd1959456d5d65666aa09
nikitalaudari41 <2070241@student.bic.edu.np>
2024-05-18 16:13:22 +0545

    trgg

--- mediq/mediq/views.py
+++ mediq/mediq/views.py
@@ -65,26 +65,5 @@ def patreg(request):
       en= Patientreg(first_name=first_name, last_name=last_name, age=age, email=email, phone=phone, message=message, gender=gender, hospital=hospital , doctor=doctor )
       en.save()
     return render(request,"patregister.html")
-<<<<<<< HEAD
 def hospreg(request):
     return render(request,'hospregister.html')
-=======
-
-def patlogin(request):
-    return render(request,"patlogin.html")
-
-def hosplogin(request):
-    return render(request,"hosplogin.html")
-
-def aboutus(request):
-    return render(request,"aboutus.html")
-
-
-
-
-
-
-
-
-    
->>>>>>> fa89ebb5777f95629d8c6f84b548fb17d7823e92
