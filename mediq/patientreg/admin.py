b22707ef4410473cccc43f2e23163c1a11e6e22d
bidishacpg <bidishachapagai@gmail.com>
2024-06-29 22:07:56 +0545

    ngg

--- mediq/patientreg/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from patientreg.models import Patientreg
-class patientregadmin(admin.ModelAdmin):
-    list_display=('id','first_name','last_name','email','age','gender','password','address','message','phone','photo')
-admin.site.register(Patientreg,patientregadmin)
