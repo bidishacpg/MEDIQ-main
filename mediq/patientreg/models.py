9c9e883218df275dfef1c2a2edb5030f58e8f95c
bidishacpg <bidishachapagai@gmail.com>
2024-06-30 12:53:18 +0545

    ufff

---
+++ mediq/patientreg/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from tinymce.models import HTMLField
+class Patientreg(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    email=models.CharField(max_length=50)
+    age=models.CharField(max_length=50)
+    password=models.CharField(max_length=50)
+    gender=models.CharField(max_length=50)
+    address=models.CharField(max_length=50)
+    message=models.TextField()
+    phone=models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='patient/',max_length=250,null=True,default=None)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
